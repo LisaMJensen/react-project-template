5bb5b3a3051750524c5810ce1e82fbb92ab6a09c
Student <example@example.com>
2019-09-12 08:25:12 -0700

    add components folder, app file, and initial base imports

---
+++ src/components/App.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import Header from './Header';
+import { Switch, Route } from 'react-router-dom';
+import { v4 } from 'uuid';
+import Moment from 'moment';
--- src/index.jsx
+++ src/index.jsx
@@ -3,3 +3,23 @@ import { HashRouter } from 'react-router-dom';
 import { Switch, Route } from 'react-router-dom';
 import { v4 } from 'uuid'; //for assigning ID's
 import Moment from 'moment'; //for working with dates/time
+
+const render = (Component) => {
+    ReactDOM.render(
+        <AppContainer>
+            <HashRouter>
+                <Component />
+            </HashRouter>
+        </AppContainer>,
+        document.getElementById('react-app-root')
+    );
+};
+
+render(App);
+/*eslint-disable */
+if (module.hot) {
+    module.hot.accept('./components/App', () => {
+        render(App);
+    });
+}
+/*eslint-enable */
