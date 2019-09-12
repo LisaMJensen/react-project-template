74d5f5521fa49213b3683ff53ce729782ff2b7af
Student <example@example.com>
2019-09-12 08:16:03 -0700

    add UUID and Moment.js packages

---
+++ src/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import { HashRouter } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
+import { v4 } from 'uuid'; //for assigning ID's
+import Moment from 'moment'; //for working with dates/time
